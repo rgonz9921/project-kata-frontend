25fad2d21c65a7f72da9b45d7bf3cdf8986ee688
Gonzalez, Andres <rgonz17@bancodebogota.com.co>
2025-03-22 21:01:25 -0500

    Basic project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {enableTracing: false, useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
